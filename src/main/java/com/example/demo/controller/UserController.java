30fad0900f00cfef590f1936a388b787bdbd103f
Varun Reddi <37947487+varunreddi@users.noreply.github.com>
2021-12-05 09:52:04 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("/info")
+	public String userInfo()
+	{
+		return "Hello war";
+	}
+}
